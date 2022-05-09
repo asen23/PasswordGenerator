387b232edc840a9e3bc1a4dc821eff9b90e312e8
asen23 <andersenlim23@gmail.com>
2022-05-09 14:45:32 +0700

    feat: add input component

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  modelValue: boolean;
+}>();
+
+const emit = defineEmits(["update:modelValue"]);
+
+function updateValue(value: boolean) {
+  emit("update:modelValue", value);
+}
+</script>
+
+<template>
+  <div>
+    <input
+      type="checkbox"
+      name="{{label}}"
+      id=""
+      :checked="modelValue"
+      @change="updateValue(($event.target as HTMLInputElement).checked)"
+    />
+    &nbsp;
+    <label for="{{label}}">{{ label }}</label>
+  </div>
+</template>
---
+++ src/components/NumberInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  modelValue: number;
+}>();
+
+const emit = defineEmits(["update:modelValue"]);
+
+function updateValue(value: string) {
+  const num = parseInt(value);
+  emit("update:modelValue", isNaN(num) ? 20 : num);
+}
+</script>
+
+<template>
+  <div>
+    <label for="{{label}}">{{ label }}</label>
+    &nbsp;
+    <input
+      type="number"
+      name="{{label}}"
+      min="1"
+      id=""
+      :value="modelValue"
+      @input="updateValue(($event.target as HTMLInputElement).value)"
+    />
+  </div>
+</template>
